eade09a91e5629c0a45a1ea2fd91af63163353bb
Melih Coban <melihcoban99@hotmail.com>
2019-04-11 20:27:22 +0000

    Made a function

--- examresults.py
+++ examresults.py
@@ -8,11 +8,8 @@ print("Type without @ogr.deu.edu.tr")
 username = input("Username: ")
 password = getpass.getpass("Password: ")
 
-print("Select the semester you would like to look.")
-print("1- 2017-2018 Fall")
-print("2- 2017-2018 Spring")
-print("3- 2018-2019 Fall")
-print("4- 2018-2019 Spring")
+print("Select the semester!\n1- 2017-2018 Fall\n2- 2017-2018 Spring\n3- 2018-2019 Fall\n4- 2018-2019 Spring\n")
+
 
 menuChoice = int(input("Menu: "))
 if menuChoice == 1:
@@ -23,6 +20,7 @@ elif menuChoice == 3:
     semesterID = int(253)
 elif menuChoice == 4:
     semesterID = int(254)
+print("-"*30)
 
 #Post data for debis.deu.edu.tr
 
@@ -45,55 +43,70 @@ result = session_requests.post(
 )
 response = result.text
 
-#Find course identities and find exam results accourding to it
+#Find course identities in the source code and find exam results accourding to it
 
 courseIdentity = re.findall(r'[A-Z]{1}_[A-Z]{2}\s_[0-9]{1}_\d*_\d*_\d*_', response)
-
-for identity in courseIdentity:
-    course = session_requests.post(
-            login_url, 
-            data = {
-            "ogretim_donemi_id": semesterID,
-            "ders": identity
-    }
-    )
-    courseResult = re.findall(r'<td align=\Wcenter\W>([0-9]{2}|[0-9]{3})<\/td>', course.text)
-    courseGrade = re.findall(r'<td align=\Wcenter\W>([A-Z]{2}\s)<\/td>', course.text)
-    courseName = re.findall(r'(?<=b>)\w*\s\d*\s-\s([^<]+)', course.text)
-    examNumber = len(courseResult)
-
-# Check for Turkish character in the course name, if its True change it.
-
-    if re.search(r'Ý', str(courseName)) is not None:
-        courseName = re.sub(r'Ý', r'İ', str(courseName))
-        courseName = re.sub(r'ý', r'ı', str(courseName))
-        courseName = re.sub(r'\[\'', r'', str(courseName))
-        courseName = re.sub(r'\'\]', r'', str(courseName))
+if not courseIdentity:
+    courseIdentity = re.findall(r'[A-Z]{1}__\d*_\d*_\d*_\d*_', response)
 
 
-    if examNumber > 5:
-        print (', '.join(courseName))
-        print("Midterm Result: " + courseResult[1])
-        print("Final Result: " + courseResult[5])
-        print("Additional Result: " + courseResult[3])
-        print("Grade: " + ', '.join(courseGrade))
-        print("-"*20)
-    elif examNumber > 3:
-        if re.search(r'İ', str(courseName)) is not None:
-            print(courseName)
-            print("Midterm Result: " + courseResult[1])
-            print("Final Result: " + courseResult[3])
-            print ("Grade: " + ', '.join(courseGrade))
-            print("-"*20)
-        else:
+def http_post(courseIdentity):
+    for identity in courseIdentity:
+        course = session_requests.post(
+                login_url, 
+                data = {
+                "ogretim_donemi_id": semesterID,
+                "ders": identity
+        }
+        )
+        courseResult = re.findall(r'<td align=\Wcenter\W>([0-9]{2}|[0-9]{3})<\/td>', course.text)
+        courseGrade = re.findall(r'<td align=\Wcenter\W>([A-Z]{2}\s)<\/td>', course.text)
+        courseName = re.findall(r'(?<=b>)\w*\s\d*\s-\s([^<]+)', course.text)
+        examNumber = len(courseResult)
+
+    # Check for Turkish character in the course name, if its True change it.
+
+        if re.search(r'Ý', str(courseName)) is not None:
+            courseName = re.sub(r'Ý', r'İ', str(courseName))
+            courseName = re.sub(r'ý', r'ı', str(courseName))
+            courseName = re.sub(r'\[\'', r'', str(courseName))
+            courseName = re.sub(r'\'\]', r'', str(courseName))
+
+        if examNumber > 5:
             print (', '.join(courseName))
             print("Midterm Result: " + courseResult[1])
-            print("Final Result: " + courseResult[3])
-            print ("Grade: " + ', '.join(courseGrade))
+            print("Final Result: " + courseResult[5])
+            print("Additional Result: " + courseResult[3])
+            print("Grade: " + ', '.join(courseGrade))
             print("-"*20)
-    elif examNumber <= 2:
-        print (', '.join(courseName))
-        print("Midterm Result: " + courseResult[1])
-        print("-"*20)
-    else:
-        print("Result did not announced yet")
+        elif examNumber > 3:
+            if re.search(r'İ', str(courseName)) is not None:
+                print(courseName)
+                print("Midterm Result: " + courseResult[1])
+                print("Final Result: " + courseResult[3])
+                print ("Grade: " + ', '.join(courseGrade))
+                print("-"*20)
+            else:
+                print (', '.join(courseName))
+                print("Midterm Result: " + courseResult[1])
+                print("Final Result: " + courseResult[3])
+                print ("Grade: " + ', '.join(courseGrade))
+                print("-"*20)
+        elif examNumber <= 2:
+            try:
+                if re.search(r'İ', str(courseName)) is not None:
+                    print(courseName)
+                    print("Midterm Result: " + courseResult[1])
+                    print("-"*30)
+                else:
+                    print (', '.join(courseName))
+                    print("Midterm Result: " + courseResult[1])
+                    print("-"*30)
+            except IndexError:
+                print("Midterm Result: N/A")
+                print("-"*30)
+        else:
+            print("Result did not announced yet")
+
+
+http_post(courseIdentity)
