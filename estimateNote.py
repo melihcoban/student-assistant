aa93e07ab163a278c96893481f90bf07ecb79dd8
Melih Coban <melihcoban99@hotmail.com>
2019-04-11 22:01:12 +0000

    Improvisations, code length reduced in half

--- estimateNote.py
+++ estimateNote.py
@@ -2,125 +2,64 @@ import time
 import re
 from decimal import Decimal
 
-midterm = int(input("Midterm Note: "))
+
+
+midterm = int(input("Midterm Note: "))  # take user input
 print("-"*17)
 midterm = midterm * 40 / 100
-estimatedNote = []
+estimated_notes = []
 for i in range(50, 101):
     final = i * 60 / 100
     result = midterm + final
     result = round(result, 2)
     result = int(result)
-    estimatedNote.append(result)
-finalNote = []
+    estimated_notes.append(result)
+final_notes = []
 n = int(50)
-for i in range(len(estimatedNote)):
-#    print(str(n) + "                  " + str(estimatedNote[i]))
-    finalNote.append(n)
+for i in range(len(estimated_notes)):
+    final_notes.append(n)
     n = n + 1
-#    time.sleep(0.05)
 
-# Divide the estimatedNote between odds and evens (finalNote note and estimated note)
-#finalNoteNote = estimatedNote[0:][::2]
-#estimatedNote = estimatedNote[1:][::2]
+grade1 = [i for i in estimated_notes if i >= 90]
+grade2 = final_notes[(-len(grade1)):]
 
-
-# Calculate AA
-
-aa1 = [i for i in estimatedNote if i >= 90]
-#aa2 = [i for i in finalNote if i >= 90]
-aa3 = finalNote[(-len(aa1)):]
-
-if aa1:
-    print("AA: Final Note: " + str(aa3) + " Estimated Note: " + str(aa1))
+if grade1:
+    print("AA\nFinal Note: {}\nEstimated Note: {}".format(grade2, grade1))
 else:
     print("You can't get AA")
 
-# Calculate BA
+final_notes = final_notes[:len(final_notes)-(len(grade1))]
+estimated_notes = estimated_notes[:len(estimated_notes)-(len(grade1))]
+print("-"*30)
 
-estimatedNote = estimatedNote[:len(estimatedNote)-(len(aa1))]
-finalNote = finalNote[:len(finalNote)-(len(aa1))]
+# update final estimated note lists
 
-ba1 = [i for i in estimatedNote if i >= 85]
-#ba2 = [i for i in finalNote if i >= 85]
-ba3 = finalNote[(-len(ba1)):]
+def update_final(subtract, final_notes):
+    final_notes = final_notes[:len(final_notes)-(len(subtract))]
+    return final_notes
 
-if ba1:
-    print("BA: Final Note: " + str(ba3) + " Estimated Note: " + str(ba1))
-else:
-    print("You can't get BA")
+def update_estimated(subtract, estimated_notes):
+    estimated_notes = estimated_notes[:len(estimated_notes)-(len(subtract))]
+    return estimated_notes
 
+# calculation for ba bb cb cc dc dd
 
-# Calculate BB
+def estimated_note(grade1,grade,grade_limit,final_notes, estimated_notes):
 
-estimatedNote = estimatedNote[:len(estimatedNote)-(len(ba1))]
-finalNote = finalNote[:len(finalNote)-(len(ba1))]
+    grade3 = [i for i in estimated_notes if i >= grade_limit]
+    grade4 = final_notes[(-len(grade3)):]
 
-bb1 = [i for i in estimatedNote if i >= 80]
-#bb2 = [i for i in finalNote if i >= 80]
-bb3 = finalNote[(-len(bb1)):]
+    if grade3:
+        print("{}\nFinal Note: {}\nEstimated Note: {}".format(grade, grade4, grade3))
+    else:
+        print("You can't get BA")
+    return grade3
 
-if bb1:
-    print("BB: Final Note: " + str(bb3) + " Estimated Note: " + str(bb1))
-else:
-    print("You can't get BB")
+grade_list = ["BA", "BB", "CB", "CC", "DC", "DD"]
+grade_number = [85, 80, 75, 70, 65, 60]
 
-
-# Calculate CB
-
-estimatedNote = estimatedNote[:len(estimatedNote)-(len(bb1))]
-finalNote = finalNote[:len(finalNote)-(len(bb1))]
-
-cb1 = [i for i in estimatedNote if i >= 75]
-#cb2 = [i for i in finalNote if i >= 75]
-cb3 = finalNote[(-len(cb1)):]
-
-if cb1:
-    print("CB: Final Note: " + str(cb3) + " Estimated Note: " + str(cb1))
-else:
-    print("You can't get CB")
-
-
-# Calculate CC
-
-estimatedNote = estimatedNote[:len(estimatedNote)-(len(cb1))]
-finalNote = finalNote[:len(finalNote)-(len(cb1))]
-
-cc1 = [i for i in estimatedNote if i >= 70]
-#cc2 = [i for i in finalNote if i >= 70]
-cc3 = finalNote[(-len(cc1)):]
-
-if cc1:
-    print("CC: Final Note: " + str(cc3) + " Estimated Note: " + str(cc1))
-else:
-    print("You can't get CC")
-
-
-# Calculate DC
-
-estimatedNote = estimatedNote[:len(estimatedNote)-(len(cc1))]
-finalNote = finalNote[:len(finalNote)-(len(cc1))]
-
-dc1 = [i for i in estimatedNote if i >= 65]
-#dc2 = [i for i in finalNote if i >= 65]
-dc3 = finalNote[(-len(dc1)):]
-
-if dc1:
-    print("DC: Final Note: " + str(dc3) + " Estimated Note: " + str(dc1))
-else:
-    print("You can't get DC")
-
-
-# Calculate DD
-
-estimatedNote = estimatedNote[:len(estimatedNote)-(len(dc1))]
-finalNote = finalNote[:len(finalNote)-(len(dc1))]
-
-dd1 = [i for i in estimatedNote if i >= 60]
-#dd2 = [i for i in finalNote if i >= 60]
-dd3 = finalNote[(-len(dd1)):]
-
-if dd1:
-    print("DD: Final Note: " + str(dd3) + " Estimated Note: " + str(dd1))
-else:
-    print("You can't get DD")
+for i in range(len(grade_list)):
+    grade3 = estimated_note(grade1, grade_list[i], grade_number[i], final_notes, estimated_notes)
+    final_notes = update_final(grade3, final_notes)
+    estimated_notes = update_estimated(grade3, estimated_notes)
+    print("-"*50)
