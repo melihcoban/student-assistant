9e596d9aacb24806b330a3a942e370325950b5ef
Melih Coban <melihcoban99@hotmail.com>
2019-04-15 13:23:40 +0000

    Changed variable names according to PEP8

--- uni_exam_results.py
+++ uni_exam_results.py
@@ -10,15 +10,15 @@ username = input("Username: ")
 password = getpass.getpass("Password: ")
 print("Select the semester!\n1- 2017-2018 Fall\n2- 2017-2018 Spring\n3- 2018-2019 Fall\n4- 2018-2019 Spring\n")
 
-menuChoice = int(input("Menu: "))
-if menuChoice == 1:
-    semesterID = int(243)
-elif menuChoice == 2:
-    semesterID = int(244)
-elif menuChoice == 3:
-    semesterID = int(253)
-elif menuChoice == 4:
-    semesterID = int(254)
+menu_choice = int(input("Menu: "))
+if menu_choice == 1:
+    semester_id = int(243)
+elif menu_choice == 2:
+    semester_id = int(244)
+elif menu_choice == 3:
+    semester_id = int(253)
+elif menu_choice == 4:
+    semester_id = int(254)
 print("-"*30)
 
 #Post data for debis.deu.edu.tr
@@ -28,7 +28,7 @@ payload = {
         "emailHost": "ogr.deu.edu.tr",
 	"password": password,
 	"tamam": "Gönder",
-	"ogretim_donemi_id": semesterID
+	"ogretim_donemi_id": semester_id
 }
 
 #Starting session because we will need GET requests later
@@ -44,62 +44,62 @@ response = result.text
 
 #Find course identities in the source code and find exam results accourding to it
 
-courseIdentity = re.findall(r'[A-Z]{1}_[A-Z]{2}\s_[0-9]{1}_\d*_\d*_\d*_', response)
-if not courseIdentity:
-    courseIdentity = re.findall(r'[A-Z]{1}__\d*_\d*_\d*_\d*_', response)
+course_identity = re.findall(r'[A-Z]{1}_[A-Z]{2}\s_[0-9]{1}_\d*_\d*_\d*_', response)
+if not course_identity:
+    course_identity = re.findall(r'[A-Z]{1}__\d*_\d*_\d*_\d*_', response)
 
 
-def http_post(courseIdentity):
-    for identity in courseIdentity:
+def http_post(course_identity):
+    for identity in course_identity:
         course = session_requests.post(
                 login_url, 
                 data = {
-                "ogretim_donemi_id": semesterID,
+                "ogretim_donemi_id": semester_id,
                 "ders": identity
         }
         )
-        courseResult = re.findall(r'<td align=\Wcenter\W>([0-9]{2}|[0-9]{3})<\/td>', course.text)
-        courseGrade = re.findall(r'<td align=\Wcenter\W>([A-Z]{2}\s)<\/td>', course.text)
-        courseName = re.findall(r'(?<=b>)\w*\s\d*\s-\s([^<]+)', course.text)
-        examNumber = len(courseResult)
+        course_result = re.findall(r'<td align=\Wcenter\W>([0-9]{2}|[0-9]{3})<\/td>', course.text)
+        course_grade = re.findall(r'<td align=\Wcenter\W>([A-Z]{2}\s)<\/td>', course.text)
+        course_name = re.findall(r'(?<=b>)\w*\s\d*\s-\s([^<]+)', course.text)
+        exam_number = len(course_result)
 
     # Check for Turkish character in the course name, if its True change it.
 
-        if re.search(r'Ý', str(courseName)) is not None:
-            courseName = re.sub(r'Ý', r'İ', str(courseName))
-            courseName = re.sub(r'ý', r'ı', str(courseName))
-            courseName = re.sub(r'\[\'', r'', str(courseName))
-            courseName = re.sub(r'\'\]', r'', str(courseName))
+        if re.search(r'Ý', str(course_name)) is not None:
+            course_name = re.sub(r'Ý', r'İ', str(course_name))
+            course_name = re.sub(r'ý', r'ı', str(course_name))
+            course_name = re.sub(r'\[\'', r'', str(course_name))
+            course_name = re.sub(r'\'\]', r'', str(course_name))
 
-        if examNumber > 5:
-            print (', '.join(courseName))
-            print("Midterm Result: " + courseResult[1])
-            print("Final Result: " + courseResult[5])
-            print("Additional Result: " + courseResult[3])
-            print("Grade: " + ', '.join(courseGrade))
+        if exam_number > 5:
+            print (', '.join(course_name))
+            print("Midterm Result: " + course_result[1])
+            print("Final Result: " + course_result[5])
+            print("Additional Result: " + course_result[3])
+            print("Grade: " + ', '.join(course_grade))
             print("-"*20)
-        elif examNumber > 3:
-            if re.search(r'İ', str(courseName)) is not None:
-                print(courseName)
-                print("Midterm Result: " + courseResult[1])
-                print("Final Result: " + courseResult[3])
-                print ("Grade: " + ', '.join(courseGrade))
+        elif exam_number > 3:
+            if re.search(r'İ', str(course_name)) is not None:
+                print(course_name)
+                print("Midterm Result: " + course_result[1])
+                print("Final Result: " + course_result[3])
+                print ("Grade: " + ', '.join(course_grade))
                 print("-"*20)
             else:
-                print (', '.join(courseName))
-                print("Midterm Result: " + courseResult[1])
-                print("Final Result: " + courseResult[3])
-                print ("Grade: " + ', '.join(courseGrade))
+                print (', '.join(course_name))
+                print("Midterm Result: " + course_result[1])
+                print("Final Result: " + course_result[3])
+                print ("Grade: " + ', '.join(course_grade))
                 print("-"*20)
-        elif examNumber <= 2:
+        elif exam_number <= 2:
             try:
-                if re.search(r'İ', str(courseName)) is not None:
-                    print(courseName)
-                    print("Midterm Result: " + courseResult[1])
+                if re.search(r'İ', str(course_name)) is not None:
+                    print(course_name)
+                    print("Midterm Result: " + course_result[1])
                     print("-"*30)
                 else:
-                    print (', '.join(courseName))
-                    print("Midterm Result: " + courseResult[1])
+                    print (', '.join(course_name))
+                    print("Midterm Result: " + course_result[1])
                     print("-"*30)
             except IndexError:
                 print("Midterm Result: N/A")
@@ -108,4 +108,4 @@ def http_post(courseIdentity):
             print("Result did not announced yet")
 
 
-http_post(courseIdentity)
+http_post(course_identity)
